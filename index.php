4fde026127261b3a62e539f5995d2c41eaf4e655
m.muntha <munthasir911@gmail.com>
2019-04-30 13:32:10 +0530

    fixd bug in header

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php    
+
+
+echo $macAddr;
+function GetClientMac(){
+    $macAddr=false;
+    $arp=`arp -n`;
+    $lines=explode("\n", $arp);
+
+    foreach($lines as $line){
+        $cols=preg_split('/\s+/', trim($line));
+
+        if ($cols[0]==$_SERVER['REMOTE_ADDR']){
+            $macAddr=$cols[2];
+        }
+    }
+
+    return $macAddr;
+}
+
+
+
+
+?>
